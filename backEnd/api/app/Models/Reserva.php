7fe42a01f2abc4ae464f1631002706aa33fd84d4
dael10 <83776067+dael10@users.noreply.github.com>
2021-10-10 17:05:48 -0500

    component

--- backEnd/api/app/Models/Reserva.php
+++ backEnd/api/app/Models/Reserva.php
@@ -9,7 +9,7 @@ class Reserva extends Model
 {
     public $timestamps = false;
     protected $table = 'reservas';
-    protected $fillable = ['id_habitacion','cant_adultos','cant_ninos','mascota','recogida', 'fecha_ingreso', 'fecha_salida','com_ingreso','calificacion','com_salida','doc_cliente'];
+    protected $fillable = ['habitacion_id','cant_adultos','cant_ninos','mascota','recogida', 'fecha_ingreso', 'fecha_salida','com_ingreso','calificacion','com_salida','doc_cliente'];
 
     public function cliente(){
         return $this->belongsTo(Cliente::class);
