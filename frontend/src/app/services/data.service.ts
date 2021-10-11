d599061827f78acc6717cd9fcf65a034dda20a1e
dael10 <83776067+dael10@users.noreply.github.com>
2021-10-10 23:31:38 -0500

    AjustesBackend

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private API = 'http://127.0.0.1:8000/api';
+
+  constructor(
+    private http: HttpClient
+    ) { }
+
+  signup(data: Object): Observable<any> {
+    return this.http.post(this.API + '/Cliente/signup', data);
+  }
+
   saludar(){
     alert('Hola');
   }
